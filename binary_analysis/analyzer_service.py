149aa0e2d3bbf40b79e1eab65b952f687fee05da
lzbaer <lzbaer@asu.edu>
2017-05-01 14:30:27 -0700

    Connect vulnerable function calls with program functions

--- binary_analysis/analyzer_service.py
+++ binary_analysis/analyzer_service.py
@@ -15,13 +15,38 @@ def path_exploit():
 def binary_category():
 	return 0
 def containsVuln(addresses):
-	vulnLine = os.popen('objdump -d a.out | grep -oP "\d.*(strcpy)"').read()
+	vulnLine = os.popen('objdump -d a.out | grep -oP "\d.*(strcpy|printf)"').read()
 	vulnCalls = vulnLine.split("\n") 
-	onlystrcpy = list(filter(lambda x:"\t" in x, vulnCalls))
-	return onlystrcpy
+	vulninstrList = list(filter(lambda x:"\t" in x, vulnCalls))
+	vulnAddr = []
+	for i in vulninstrList:
+		addr = i[0:7]
+		vulnAddr.append(int("0x" + addr, 16))
+	functionAddr = []
+	for i in addresses:
+		addr = i[0:8]
+		#print addr
+		if addr != "":
+			functionAddr.append(int("0x" + addr, 16))
+	#print vulninstrList
+	#print vulnAddr
+
+	#print addresses
+	#print functionAddr
+	toPrint = []
+	for i in range(0, len(vulnAddr)):
+		minJ = -1
+		for j in range(0, len(functionAddr)):
+			function = 9999999
+			if (vulnAddr[i] > functionAddr[j] and vulnAddr[i]-functionAddr[j] < function):
+				function = vulnAddr[i]-functionAddr[j]
+				minJ = j
+		
+		toPrint.append(str(vulninstrList[i] + "\n") + " belongs in\n" + str(addresses[minJ]))		
+	return toPrint
 
 vuln_funs = filter(None,os.popen('objdump -d '+ sys.argv[1] +  ' | grep -oP "(strcpy|memcpy|scanf|printf)"').read().split('\n'))
-print vuln_funs
+#print vuln_funs
 vuln_set = set(vuln_funs)
 print "This program has these vulnerable functions:\n" + str(vuln_set)
 
@@ -32,8 +57,8 @@ listAddresses = startAddresses.split("\n")
 #print listAddresses 
 
 goodAddresses = list(filter(lambda x: not "@" in x, listAddresses))
-print goodAddresses
+#print goodAddresses
 
 test = containsVuln(goodAddresses)
-print test
-print "Calls:\n" + str(test)
+for i in test:
+	print i + "\n"
